a6ce9cde01dae91e54dc520de05cc9a9e6b71d97
JelleDBZ <jelle.debuyzere@hotmail.be>
2018-05-05 23:21:19 +0200

    able to read data from biometricstationLogic

--- BiometricStationFX/src/biometricstationfx/FXMLDocumentController.java
+++ BiometricStationFX/src/biometricstationfx/FXMLDocumentController.java
@@ -11,6 +11,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import mqttservice.*;
 
 /**
@@ -18,27 +19,32 @@ import mqttservice.*;
  * @author jelle
  */
 public class FXMLDocumentController implements Initializable, IMqttMessageHandler {
-    
+
     @FXML
     private Label label;
-    
-    Service service;
-    
+    @FXML
+    private TextArea area;
+
+    private Service service = new Service("Jelle", "Temperature");
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        service = new Service("Jelle", "temperature");
-        service.IMqttMessageHandler(this);
-    }    
+
+        service.setMessageHandler(this);
+    }
 
     @Override
     public void messageArrived(String channel, String message) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (channel.equals("Temperature")) {
+
+            System.out.println("Message: " + message);
+
+        }
     }
-    
 }
