a95757196f5ce8d92fc38f0232d5d1cee9f9e556
JelleDBZ <jelle.debuyzere@hotmail.be>
2018-05-06 21:31:59 +0200

    Add working FXML file with functionality to print data in textfields

--- BiometricStationFX/src/biometricstationfx/FXMLDocumentController.java
+++ BiometricStationFX/src/biometricstationfx/FXMLDocumentController.java
@@ -10,8 +10,11 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+//import javafx.scene.chart.LineChart;
+//import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import mqttservice.*;
 
 /**
@@ -24,8 +27,25 @@ public class FXMLDocumentController implements Initializable, IMqttMessageHandle
     private Label label;
     @FXML
     private TextArea area;
-
-    private Service service = new Service("Jelle", "Temperature");
+    @FXML
+    private TextField tempField;
+    @FXML
+    private TextField heartField;
+    @FXML
+    private TextField accXField;
+    @FXML 
+//    private LineChart temperatureChart;
+    private Double temp;
+    
+    
+    private double xValue = 0.0;
+    
+    
+    //private XYChart.Series temperatureValues;
+    private Service serviceTemp= new Service("Jelle", "Temperature");
+    private Service serviceAcc= new Service("Jelle", "Accelerometer");
+    private Service servicePulse= new Service("Jelle", "Heartpulse");
+    
 
     @FXML
     private void handleButtonAction(ActionEvent event) {
@@ -35,16 +55,44 @@ public class FXMLDocumentController implements Initializable, IMqttMessageHandle
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        serviceTemp.setMessageHandler(this);
+        serviceAcc.setMessageHandler(this);
+        servicePulse.setMessageHandler(this);
+        
+//        temperatureValues = new XYChart.Series();
+//        temperatureValues.setName("Temperature (Celsius)");
+//        temperatureChart.getData().add(temperatureValues);
 
-        service.setMessageHandler(this);
     }
 
     @Override
-    public void messageArrived(String channel, String message) { //TODO: switch statement
-        if (channel.equals("Temperature")) {
-            System.out.println("Message: " + message);
-            area.setText(message);
+    public void messageArrived(String channel, String message) {
+//        temp = Double.parseDouble(message);
+//        System.out.println("this is a parsed data " + temp);
+//        temperatureValues.getData().add(new XYChart.Data(xValue, temp));
+//        xValue += 0.1;
+        
 
+        switch (channel) { //TODO: testing (without break; it will run but after a couple of seconds it will crash and give an arrayoutofbounds exception
+            case "Temperature":
+                 //test
+                tempField.getText().trim();
+                tempField.setText(message + "°C");
+                
+            case "Accelerometer":
+                //System.out.println(message);
+                
+            case "Heartpulse":
+                //System.out.println("Message: " + message); //test
+                heartField.getText().trim();
+                heartField.setText(message + "BPM");
+            case "default":
+                tempField.getText().trim();
+                break;
+                
         }
+
+//        if (channel.equals("Temperature")) { //other way 
+//        }
     }
 }
