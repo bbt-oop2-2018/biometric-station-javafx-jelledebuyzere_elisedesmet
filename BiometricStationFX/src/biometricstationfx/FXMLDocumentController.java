4fa7f01700b6f3b67ed300ea6cc1de2688d73145
JelleDBZ <jelle.debuyzere@hotmail.be>
2018-05-11 17:02:00 +0200

    Add complete switch case

--- BiometricStationFX/src/biometricstationfx/FXMLDocumentController.java
+++ BiometricStationFX/src/biometricstationfx/FXMLDocumentController.java
@@ -10,12 +10,14 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-//import javafx.scene.chart.LineChart;
-//import javafx.scene.chart.XYChart;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import mqttservice.*;
+import biometricstationjava.ArduinoParser;
+import biometricstationjava.SensorData;
 
 /**
  *
@@ -33,19 +35,21 @@ public class FXMLDocumentController implements Initializable, IMqttMessageHandle
     private TextField heartField;
     @FXML
     private TextField accXField;
-    @FXML 
+    @FXML
+    private TextField accYField;
+    @FXML
+    private TextField accZField;
+    @FXML
 //    private LineChart temperatureChart;
     private Double temp;
-    
-    
     private double xValue = 0.0;
-    
-    
+    private ArduinoParser parser;
+    private SensorData parsedData;
+
     //private XYChart.Series temperatureValues;
-    private Service serviceTemp= new Service("Jelle", "Temperature");
-    private Service serviceAcc= new Service("Jelle", "Accelerometer");
-    private Service servicePulse= new Service("Jelle", "Heartpulse");
-    
+    private Service serviceTemp = new Service("Jelle", "Temperature");
+    private Service serviceAcc = new Service("Jelle", "Accelerometer");
+    private Service servicePulse = new Service("Jelle", "Heartpulse");
 
     @FXML
     private void handleButtonAction(ActionEvent event) {
@@ -55,15 +59,14 @@ public class FXMLDocumentController implements Initializable, IMqttMessageHandle
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        System.out.println("hello world!");
+        System.out.println("Welcome to our Biometric datacenter!");
         serviceTemp.setMessageHandler(this);
         serviceAcc.setMessageHandler(this);
         servicePulse.setMessageHandler(this);
-        
+
 //        temperatureValues = new XYChart.Series();
 //        temperatureValues.setName("Temperature (Celsius)");
 //        temperatureChart.getData().add(temperatureValues);
-
     }
 
     @Override
@@ -72,25 +75,29 @@ public class FXMLDocumentController implements Initializable, IMqttMessageHandle
 //        System.out.println("this is a parsed data " + temp);
 //        temperatureValues.getData().add(new XYChart.Data(xValue, temp));
 //        xValue += 0.1;
-        System.out.println("temperature reading");
+        System.out.println(message);
 
         switch (channel) { //TODO: testing (without break; it will run but after a couple of seconds it will crash and give an arrayoutofbounds exception
             case "Temperature":
-                 System.out.println("temperature reading");//test
-                tempField.getText().trim();
+                System.out.println("temperature reading");//test
                 tempField.setText(message + "°C");
-                
-            case "Accelerometer":
-                //System.out.println(message);
-                
+                break;
+
+            case "Accelerometer": //parsing data maybe with json because this isn't working properly
+                parsedData = parser.parse(message);
+                accXField.setText(parsedData.getAcc_X() + "");
+                accYField.setText(parsedData.getAcc_Y() + "");
+                accZField.setText(parsedData.getAcc_Z() + "");
+                break;
+
             case "Heartpulse":
                 //System.out.println("Message: " + message); //test
-                heartField.getText().trim();
                 heartField.setText(message + "BPM");
+                break;
+                
             case "default":
                 //tempField.getText().trim();
                 break;
-                
         }
 
 //        if (channel.equals("Temperature")) { //other way 
